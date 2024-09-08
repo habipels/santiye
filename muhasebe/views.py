9efbda265950542c5af3505496df12ee389ba594
Habip ELİŞ <46339672+habipels@users.noreply.github.com>
2024-09-08 03:29:32 +0300

    #ürünler Kategorisi

--- muhasebe/views.py
+++ muhasebe/views.py
@@ -3812,3 +3812,133 @@ def fatura_goster2qr(request,id):
     content["bilgi"] = gelir_bilgisi_ver
     content["urunler"] = urunleri
     return render(request,"muhasebe_page/faturalari_goster_gider.html",content)
+
+def urunler_kategorisi(request):
+    content = sozluk_yapisi()
+    if super_admin_kontrolu(request):
+        profile =urun_kategorileri.objects.all()
+        kullanicilar = CustomUser.objects.filter(kullanicilar_db = None,is_superuser = False).order_by("-id")
+        content["kullanicilar"] =kullanicilar
+    else:
+        if request.user.kullanicilar_db:
+            a = get_object_or_none(bagli_kullanicilar,kullanicilar = request.user)
+            if a:
+                if a.izinler.urun_gorme:
+                    profile = urun_kategorileri.objects.filter(silinme_bilgisi = False,kategrori_ait_oldugu = request.user.kullanicilar_db)
+                else:
+                    return redirect("main:yetkisiz")
+            else:
+                return redirect("main:yetkisiz")
+        else:
+            profile = urun_kategorileri.objects.filter(silinme_bilgisi = False,kategrori_ait_oldugu = request.user)
+    if request.GET.get("search"):
+        search = request.GET.get("search")
+        if super_admin_kontrolu(request):
+            profile =urun_kategorileri.objects.filter(Q(kategrori_ait_oldugu__first_name__icontains = search)|Q(kategori_adi__icontains = search))
+            kullanicilar = CustomUser.objects.filter( kullanicilar_db = None,is_superuser = False).order_by("-id")
+            content["kullanicilar"] =kullanicilar
+        else:
+            if request.user.kullanicilar_db:
+                a = get_object_or_none(bagli_kullanicilar,kullanicilar = request.user)
+                if a:
+                    if a.izinler.urun_gorme:
+                        profile = urun_kategorileri.objects.filter(Q(kategrori_ait_oldugu = request.user.kullanicilar_db) & Q(kategori_adi__icontains = search)& Q(silinme_bilgisi = False))
+                    else:
+                        return redirect("main:yetkisiz")
+                else:
+                    return redirect("main:yetkisiz")
+            else:
+                profile = urun_kategorileri.objects.filter(Q(kategrori_ait_oldugu = request.user) & Q(kategori_adi__icontains = search)& Q(silinme_bilgisi = False))
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(profile, 10) # 6 employees per page
+
+    try:
+        page_obj = paginator.page(page_num)
+    except PageNotAnInteger:
+            # if page is not an integer, deliver the first page
+        page_obj = paginator.page(1)
+    except EmptyPage:
+            # if the page is out of range, deliver the last page
+        page_obj = paginator.page(paginator.num_pages)
+    content["santiyeler"] = page_obj
+    content["top"]  = profile
+    content["medya"] = page_obj
+    return render(request,"stok/urun_kategorileri.html",content)
+
+def urunler_kategorisi_ekle(request):
+    if request.POST:
+        #yetkili_adi
+        if super_admin_kontrolu(request):
+            kullanici_bilgisi  = request.POST.get("kullanici")
+            kasa_Adi   = request.POST.get("kasaadi")
+            urun_kategorileri.objects.create(kategrori_ait_oldugu = get_object_or_404(CustomUser,id = kullanici_bilgisi )
+                                ,kategori_adi = kasa_Adi
+
+                                )
+        else:
+            if request.user.kullanicilar_db:
+                a = get_object_or_none(bagli_kullanicilar,kullanicilar = request.user)
+                if a:
+                    if a.izinler.urun_olusturma:
+                        kasa_Adi   = request.POST.get("kasaadi")
+                        urun_kategorileri.objects.create(kategrori_ait_oldugu = request.user.kullanicilar_db
+                            ,kategori_adi = kasa_Adi)
+                    else:
+                        return redirect("main:yetkisiz")
+                else:
+                    return redirect("main:yetkisiz")
+            else:
+                kasa_Adi   = request.POST.get("kasaadi")
+                urun_kategorileri.objects.create(kategrori_ait_oldugu = request.user
+                    ,kategori_adi = kasa_Adi)
+
+    return redirect("accounting:urunler_kategorisi")
+def urunler_kategorisi_sil(request):
+    content = {}
+    if request.POST:
+        id = request.POST.get("buttonId")
+    if super_admin_kontrolu(request):
+        kullanici_bilgisi  = request.POST.get("kullanici")
+        proje_tip_adi   = request.POST.get("yetkili_adi")
+        urunler.objects.filter(id = id).update(silinme_bilgisi = True)
+    else:
+        if request.user.kullanicilar_db:
+            a = get_object_or_none(bagli_kullanicilar,kullanicilar = request.user)
+            if a:
+                if a.izinler.urun_silme:
+                    urun_kategorileri.objects.filter(kategrori_ait_oldugu = request.user.kullanicilar_db,id = id).delete()
+                else:
+                    return redirect("main:yetkisiz")
+            else:
+                return redirect("main:yetkisiz")
+        else:
+            urun_kategorileri.objects.filter(kategrori_ait_oldugu = request.user,id = id).delete()
+    return redirect("accounting:urunler_kategorisi")
+
+def urunler_kategorisi_duzenle(request):
+    content = {}
+    if request.POST:
+        id = request.POST.get("buttonId")
+    if super_admin_kontrolu(request):
+        kullanici_bilgisi  = request.POST.get("kullanici")
+        proje_tip_adi   = request.POST.get("kasaadi")
+        urun_kategorileri.objects.filter(id = id).update(kategrori_ait_oldugu = get_object_or_404(CustomUser,id = kullanici_bilgisi ) ,kategori_adi = proje_tip_adi)
+            
+    else:
+        if request.user.kullanicilar_db:
+            a = get_object_or_none(bagli_kullanicilar,kullanicilar = request.user)
+            if a:
+                if a.izinler.urun_guncelleme:
+                    proje_tip_adi   = request.POST.get("kasaadi")
+                    
+                    urun_kategorileri.objects.filter(kategrori_ait_oldugu = request.user.kullanicilar_db,id = id).update(kategori_adi = proje_tip_adi
+                            )
+                else:
+                    return redirect("main:yetkisiz")
+            else:
+                return redirect("main:yetkisiz")
+        else:
+            proje_tip_adi   = request.POST.get("kasaadi")
+            urun_kategorileri.objects.filter(kategrori_ait_oldugu = request.user,id = id).update(kategori_adi = proje_tip_adi
+                            )
+    return redirect("accounting:urunler_kategorisi")
