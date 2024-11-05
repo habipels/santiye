279470cd3572416ad21a6f492f8b7ce25ce5d671
Habip ELİŞ <46339672+habipels@users.noreply.github.com>
2024-11-05 15:27:53 +0300

    #soranice

--- po.py
+++ po.py
@@ -2,18 +2,18 @@ import polib
 from deep_translator import GoogleTranslator
 
 # Load the .po file using polib
-po = polib.pofile(r"C:\Users\habip\Documents\GitHub\santiye\django_translated.po")
-
+sorani = polib.pofile(r"C:\Users\habip\Documents\GitHub\santiye\sorani.po")
+ingilizce = polib.pofile(r"C:\Users\habip\Documents\GitHub\santiye\ingilizce.po")
 # Translator instance for Turkish to English
 translator = GoogleTranslator(source='auto', target='tr')
 
-# Translate each entry
-for entry in po:
-    if entry.msgid:  # Only translate if there's a msgstr (i.e., a Turkish translation exists)
-        translated_text = translator.translate(entry.msgid)
-        entry.msgstr = entry.msgid
+for i in ingilizce:
+    if i.msgstr:
+        i.msgid = i.msgstr
+        i.msgstr = ""
+
 # Save the translated file
-translated_po_file_path = r'C:\Users\habip\Documents\GitHub\santiye\dsjango_translated.po'
-po.save(translated_po_file_path)
+translated_po_file_path = r'C:\Users\habip\Documents\GitHub\santiye\donustu.po'
+ingilizce.save(translated_po_file_path)
 
 translated_po_file_path  # Return the path to the translated .po file
