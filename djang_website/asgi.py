a6334a21193c7440766805b658a77afff0e16063
Habip ELİŞ <46339672+habipels@users.noreply.github.com>
2025-04-01 11:02:24 +0300

    #caht sorunu bitti

--- djang_website/asgi.py
+++ djang_website/asgi.py
@@ -5,16 +5,16 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
-from users.routing import websocket_urlpatterns
+import users.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djang_website.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AllowedHostsOriginValidator(
+    'http': get_asgi_application(),
+    'websocket': AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
-                websocket_urlpatterns
+                users.routing.websocket_urlpatterns
             )
         )
     ),
--- djang_website/settings.py
+++ djang_website/settings.py
@@ -160,10 +160,7 @@ if DEPLOY__:
     ASGI_APPLICATION = 'djang_website.asgi.application'
     CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
     },
 }
     # Password validation
@@ -258,10 +255,7 @@ else:
     ASGI_APPLICATION = 'djang_website.asgi.application'
     CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
     },
 }
 
